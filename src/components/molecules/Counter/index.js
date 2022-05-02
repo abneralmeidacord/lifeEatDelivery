a011ce7a5ec1b43a19dba6c322d378b502e03fdb
Abner Almeida <85902242+abneralmeidacord@users.noreply.github.com>
2022-05-02 15:48:43 -0300

    ~COUNTER~ Minus disabled count ===0

--- src/components/molecules/Counter/index.js
+++ src/components/molecules/Counter/index.js
@@ -14,11 +14,14 @@ export const Counter = ({ props }) => {
       h={24}
       w={74}
       {...props}>
-      <Touchable w="30%" onClick={() => setCount(count - 1)}>
+      <Touchable
+        w="30%"
+        onClick={() => setCount(count - 1)}
+        disabled={count === 0}>
         <Icon feather name="minus" size={14} />
       </Touchable>
       <Box>
-       <Text.CardTitle>{count}</Text.CardTitle>  
+        <Text.CardTitle>{count}</Text.CardTitle>
       </Box>
       <Touchable w="30%" onClick={() => setCount(count + 1)}>
         <Icon feather name="plus" size={14} />
